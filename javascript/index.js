1d99b79afe0cb9cdbfca4c19407d03a60698186b
JulesHeller <julesheller0@gmail.com>
2021-11-08 22:45:29 +0100

    animations sur l'accueil

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,34 @@
+var a = document.querySelector("#a");
+var z = document.querySelector("#z");
+var dessin = document.querySelector(".jules");
+
+a.classList.toggle("lettreAnim");
+
+document.querySelector(".lettreAnim").addEventListener("click", lettreAnim);
+
+function lettreAnim(){
+    a.classList.toggle("lettreAnim");
+    z.classList.toggle("lettreAnim");
+}
+
+a.addEventListener("click", anim1);
+
+function anim1(){
+    dessin.classList.remove("julesAnim2");
+    dessin.offsetTop;
+    dessin.classList.add("julesAnim");
+}
+
+z.addEventListener("click", anim2);
+
+function anim2(){
+    dessin.classList.remove("julesAnim");
+    dessin.offsetTop;
+    dessin.classList.add("julesAnim2");
+}
+
+/* MENU */
+
 var burger = document.querySelector(".burgerAbsolu");
 var film = document.querySelector(".filmCache");
 var menu = document.querySelector(".menu");
