ccda207475b0efa0e67748ddb00f07b5f8934cb3
JulesHeller <julesheller0@gmail.com>
2021-12-09 19:14:27 +0100

    menu update

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ document.querySelector("#j1").addEventListener("click", montrerMontre);
 document.querySelector("#j4").addEventListener("click", montrerMontre);
 
 function montrerMontre() {
-    document.querySelector(".montre").classList.toggle("montreVisible")
+    document.querySelector("body").classList.toggle("clockBody")
 }
 
 /* Fonction Montre */
@@ -13,7 +13,7 @@ document.querySelector(".montre").addEventListener("click", imgOpacity);
 
 function imgOpacity() {
     document.querySelector(".jules").classList.toggle("julesFlat");
-    
+
     document.querySelector(".montre2").classList.toggle("montreAlt");
 }
 
@@ -25,3 +25,7 @@ document.querySelector(".film").addEventListener("click", showMenu);
 function showMenu() {
     document.querySelector("body").classList.toggle("menuOuvert");
 }
+
+document.querySelector("#pf").addEventListener("click", function () {
+    document.querySelector(".portfolio").classList.toggle("pfOuvert")
+})
